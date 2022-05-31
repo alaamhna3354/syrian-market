957533195bddf9c2f658e09425a248bfd51dc1df
alaamhna3354 <76902248+alaamhna3354@users.noreply.github.com>
2022-05-31 04:36:46 -0400

    edit header

--- resources/views/user/layouts/side-notify.blade.php
+++ resources/views/user/layouts/side-notify.blade.php
@@ -104,7 +104,76 @@
                         </div>
                     </li>
                 </ul>
+                    <div class="side-fotter d-flex justify-content-around">
+                    <div class="push-notification dropdown " id="pushNotificationArea">
 
+<a class="nav-link dropdown-toggle pl-md-3 position-relative" href="javascript:void(0)"
+   id="bell" role="button" data-toggle="dropdown" aria-haspopup="true"
+   aria-expanded="false">
+    <span><i class="far fa-bell bell-font"></i></span>
+    <span class="badge notify-no rounded-circle" style="background-color: #fe5916;color:#fff" v-cloak>@{{ items.length }}</span>
+
+</a>
+
+<div class="right-dropdown dropdown-menu dropdown-menu-right mailbox animated bounceInDown">
+    <ul class="list-style-none">
+        <li>
+            <div class="scrollable message-center notifications position-relative">
+
+                <!-- Message -->
+                <a v-for="(item, index) in items"
+                   @click.prevent="readAt(item.id, item.description.link)"
+                   href="javascript:void(0)"
+                   class="message-item d-flex align-items-center border-bottom px-3 py-2">
+                                <span class="btn-success text-white rounded-circle btn-circle">
+                                    <i :class="item.description.icon" class="text-white"></i>
+                                </span>
+
+                    <div class="d-inline-block v-middle pl-2">
+                        <span class="font-12  d-block text-white"  v-cloak v-html="item.description.text"></span>
+                        <span style="opacity: 0.6;" class="font-12  d-block text-white text-truncate" v-cloak>@{{ item.formatted_date }}</span>
+                    </div>
+                </a>
+
+            </div>
+        </li>
+
+        <li>
+            <a class="nav-link pt-3 text-center text-white notification-clear-btn" href="javascript:void(0);"
+               v-if="items.length > 0" @click.prevent="readAll">
+                <strong>@lang('Clear all')</strong>
+            </a>
+            <a class="nav-link pt-3 text-center text-white" href="javascript:void(0);"
+               v-else>
+                <strong>@lang('No Data found')</strong>
+            </a>
+            
+
+        </li>
+    </ul>
+</div>
+
+</div>
+            <a class="logout-item" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                                        <i data-feather="power" class="fa fa-power-off"></i>
+                                       
+                <!-- {{ __('Logout') }} -->
+            </a>
+            <a class="logout-item {{menuActive('user.profile')}}" href="{{ route('user.profile') }}">
+                                <i data-feather="user" class="svg-icon mr-2 ml-1"></i>
+                                <!-- @lang('My Profile') -->
+            </a>
+            <a class="logout-item {{menuActive('user.ticket.create')}}" href="{{ route('user.ticket.create') }}">
+                <i class="fab fa-hire-a-helper mr-2 ml-1 icon-color"></i>
+                <!-- @lang('Open Ticket') -->
+            </a>
+            <a class="logout-item {{menuActive('user.ticket.list')}}" href="{{ route('user.ticket.list') }}">
+                <i class="fas fa-ticket-alt mr-2 ml-1 icon-color"></i> 
+                <!-- @lang('Show Ticket') -->
+            </a>
+                    </div>
             </div>
     <!-- <div class="fs-wrapper">
         @foreach($notices as $notice)
