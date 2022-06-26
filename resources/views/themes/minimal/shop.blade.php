3217e914a5bcbb61244d60aba5d332c173fab429
alaamhna3354 <76902248+alaamhna3354@users.noreply.github.com>
2022-06-26 18:33:42 -0400

    edit guide

--- resources/views/themes/minimal/shop.blade.php
+++ resources/views/themes/minimal/shop.blade.php
@@ -30,21 +30,18 @@
             </div>
             <div class="row justify-content-center">
                 @foreach($categories as $category)
-                <div class="col-lg-4 col-md-6 col-sm-8">
-                    <div class="shop-item mb-45">
+                <div class="col-xl-3 col-lg-4 col-md-6 col-sm-8">
+                    <div class="popular-game-item mb-40">
+                        <div class="popular-game-thumb">
+                            <a href="{{route("user.services.show",$category->id)}}">
+                                <img src="{{ getFile(config('location.category.path').$category->image) }}" alt=""></a>
+                        </div>    
                         <a href="{{route("user.services.show",$category->id)}}">
-                        <div class="product-thumb">
-                              <img src="{{ getFile(config('location.category.path').$category->image) }}" alt="">
-                            {{--<a href="#" class="add-to-cart btn"><i class="fas fa-shopping-bag"></i>add to cart</a>--}}
+                        <div class="popular-game-overlay">
+                            <h6><span>{{$category->category_title}}</span></h6>
+                            {{--<a href="uc-details.html" class="cart"><i class="fas fa-shopping-basket"></i></a>--}}
                         </div>
                         </a>
-                        <div class="shop-item-bottom">
-                            <div class="content">
-                                <h4><a href="{{route("user.services.show",$category->id)}}">{{$category->category_title}}</a></h4>
-                                {{--<a href="#" class="cat">{{$category->category_title}}</a>--}}
-                            </div>
-                            {{--<div class="price">{{count($category->service)}} @lang('Services')</div>--}}
-                        </div>
                     </div>
                 </div>
                 @endforeach
