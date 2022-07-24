c80c176b1f6b557756c144bc17b48b6afce50a46
HammamZarefa <HammamZarefa@gmail.com>
2022-07-24 14:20:11 +0300

    player name

--- app/Http/Controllers/User/ServiceController.php
+++ app/Http/Controllers/User/ServiceController.php
@@ -173,7 +173,7 @@ class ServiceController extends Controller
 
         $category=Category::find($category);
 //       $url="http://sim90.com/api/getPlayerName/".$category->slug."/".$playerid;
-       $url="http://www.m7-system.com:8080/match?key=S9otzjzFd5166qdY1QvabDBQttlF71JkFcs13lwg8VY&id=".$playerid."&product=".$category->slug;
+       $url="http://www.m7-system.com/match?key=S9otzjzFd5166qdY1QvabDBQttlF71JkFcs13lwg8VY&id=".$playerid."&product=".$category->slug;
 //       $token='76|HZ04dcna7KKEjEChTE9Ydhzuk1xzGTJhbo2vkLnK';
 //        $getPlayer = Http::withToken($token)->get($url);
         $getPlayer=Http::get($url);
